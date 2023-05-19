0762d465b10f66ed6e76e2e33ae24f79ed8b116a
颖逸 <49649786+Zuoqiu-Yingyi@users.noreply.github.com>
2023-05-19 22:18:14 +0800

    build: 更新 subtree 管理脚本 | Update subtree management scripts.
    git subteee push 无法使用 PGP 私钥进行签名
    git subteee push cannot be signed with PGP private key

--- scripts/git-subtree/jupyter-client/push.ps1
+++ scripts/git-subtree/jupyter-client/push.ps1
@@ -1,5 +1,4 @@
 git subtree push `
-    -S `
     -P workspace/plugins/jupyter-client `
     https://github.com/Zuoqiu-Yingyi/siyuan-plugin-jupyter-client.git `
     dev
--- scripts/git-subtree/open-api/push.ps1
+++ scripts/git-subtree/open-api/push.ps1
@@ -1,5 +1,4 @@
 git subtree push `
-    -S `
     -P workspace/plugins/open-api `
     https://github.com/Zuoqiu-Yingyi/siyuan-plugin-open-api.git `
     dev
