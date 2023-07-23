aca1412783b65c41bc40eb4c302f96bc3c8ad380
颖逸 <49649786+Zuoqiu-Yingyi@users.noreply.github.com>
2023-07-23 10:44:25 +0800

    feat(monaco-editor): 添加工作区目录与文件的提示文本 | Add prompt text for workspace directories and files.

--- src/explorer/icon.ts
+++ src/explorer/icon.ts
@@ -15,7 +15,8 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import { FileTreeNodeType, type IFileTreeNode, type IFileTreeNodeStores } from "@workspace/components/siyuan/tree/file";
+import type MonacoEditorPlugin from "@/index";
+import { FileTreeNodeType, type IFileTreeNodeStores } from "@workspace/components/siyuan/tree/file";
 import { parse } from "@workspace/utils/path/browserify";
 import { get } from "svelte/store";
 
@@ -30,22 +31,36 @@ export class Icon {
         workspace: "#iconWorkspace",
     } as const;
 
+    protected readonly i18n: MonacoEditorPlugin["i18n"];
+    protected readonly map: Map<string, string>; // 路径 -> 图标 ID 映射表
+
+    constructor(
+        public readonly plugin: InstanceType<typeof MonacoEditorPlugin>, // 插件对象
+    ) {
+        this.i18n = this.plugin.i18n;
+        this.map = new Map();
+    }
+
     /* 展开节点 */
-    public static expand(node: IFileTreeNodeStores) {
+    public expand(node: IFileTreeNodeStores): void {
         if (get(node.type) === FileTreeNodeType.Folder) {
             node.icon.set(Icon.ICONS.folder_opend);
         }
     }
 
     /* 收缩节点 */
-    public static collapse(node: IFileTreeNodeStores) {
+    public collapse(node: IFileTreeNodeStores): void {
         if (get(node.type) === FileTreeNodeType.Folder) {
             node.icon.set(Icon.ICONS.folder_closed);
         }
     }
 
     /* 根据节点信息生成图标 ID */
-    public static make(type: FileTreeNodeType, path: string): string {
+    public make(type: FileTreeNodeType, path: string): string {
+        /* 若映射表中存在对应的图标, 则直接使用 */
+        const icon = this.map.get(path);
+        if (icon) return icon;
+
         switch (type) {
             case FileTreeNodeType.Root:
                 return Icon.ICONS.workspace;
